33a5ecf193caacd5961fc9fcda66e21b41db2151
DrewD1998 <Drew.Davis391998@gmail.com>
2019-01-18 01:23:39 -0500

    Lab Part 1: Int function correct. Long long function to be made.

---
+++ Lab1/Lab1Part1.cc
@@ -0,0 +1,52 @@
+/*
+Author: <Your name>
+Date last modified: <date last modified>
+Organization: ECE2036 Class
+Description:
+Describe what is done in this file.
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int intFiveN(int N){
+
+    int result = 5;
+    int count = N-1;
+
+    if(N = 0)
+    {
+        result = 0;
+    }
+
+    else
+    {
+        while(count != 0)
+        {
+            result = result * 5;
+            count = count - 1;
+        }
+    }
+    return(result);
+}
+
+
+
+
+int main()
+{   
+    int result;
+    int exponent;
+    int diff;
+    cout << "Please enter exponent value : ";
+    cin >> exponent;
+    cout << "The exponent is : " << exponent << endl;
+    result = intFiveN(exponent);
+
+    cout << "The Result is : " << result << endl;
+    diff = 2147483647 - result;
+    cout << "The Difference between Max is : " << diff << endl;
+    
+    return 0;
+}
